93b43fea3571f26374750804fd43364b4a8b7d01
ttinaguo <tina.guo@tufts.edu>
2019-02-19 16:04:34 -0500

    Update pendulum project.py

--- pendulum project.py
+++ pendulum project.py
@@ -1,1 +1,25 @@
 # pendulum project
+# Amma Agyei and Tina Guo
+
+#model a pendulum using legos and microbit
+#use physics equations to determine period(T) and angular velocity(v)
+
+import math
+import numpy as np
+
+L = .12 #m
+g = 9.8 #m/s^2
+m = 0.5 #kg
+theta_max = 180 #degrees
+theta = 172
+theta_r = math.radians(theta)
+
+def system_update(T, acc_t, vel_t, theta, t1, t2):
+    #return updated values of period, acceleration, ang velocity, angular position
+    dt = t2 - t1
+    T = (2 * math.pi) * (math.sqrt(L / g))
+    theta = theta_max * (math.sin( math.sqrt(g / L))) * dt
+    acc_t = (g * math.sin(math.radians(theta))) / m
+    vel_t = math.sqrt(2 * g * L * (1 - (math.cos(theta_r))))
+
+
