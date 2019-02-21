2cb0ce6f3ccdb46aa443a9061bc5e18f25f9b83d
Amma Agyei <agyeiamma@gmail.com>
2019-02-21 15:15:47 -0500

    Update pendulum project.py

--- pendulum project.py
+++ pendulum project.py
@@ -10,6 +10,7 @@ import numpy as np
 L = .12 #m
 g = 9.8 #m/s^2
 m = 0.5 #kg
+w = math.sqrt(g / L)
 
 def system_update(x_initial, omega_initial, time1, time2):
     #return updated values of period, acceleration, and velocity, angular position
@@ -17,6 +18,8 @@ def system_update(x_initial, omega_initial, time1, time2):
     T = (2 * np.pi) * (np.sqrt(L / g))
     omega = omega_initial * math.cos(math.sqrt(g / L) * dt )
     x_t = array_x * (np.sin(omega_initial * dt))
+    #vel_t = omega_initial * w * math.cos( w * t)
+    #acc_t = - omega_initial * w * math.sin( w * t)
     return x_t, omega
 
 def print_system(time, x_initial, omega_initial):
@@ -27,7 +30,7 @@ def print_system(time, x_initial, omega_initial):
 #initial conditions
 x_initial = [math.pi]
 array_x = np.array(x_initial)
-omega_initial = [0, math.pi/2, 20, 30, 40, math.pi / 3, math.pi / 4, math.pi / 6, math.pi]
+omega_initial = [0, math.pi/2, 20, 30, 40, math.pi / 3, math.pi / 4, math.pi / 6, math.pi, -math.pi, math.pi /2]
 time = np.linspace(0, 20, 21)
 print_system(time[0], x_initial[0], omega_initial[0])
 
