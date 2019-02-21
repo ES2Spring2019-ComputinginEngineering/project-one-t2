ebd3260a43190b4eda47304f8e8205fce525bdec
Amma Agyei <agyeiamma@gmail.com>
2019-02-21 00:41:44 -0500

    Update pendulum project.py

--- pendulum project.py
+++ pendulum project.py
@@ -10,27 +10,36 @@ import numpy as np
 L = .12 #m
 g = 9.8 #m/s^2
 m = 0.5 #kg
-time = np.linspace(0, 20, 21)
 
-
-def system_update(x_initial, time1, time2):
+def system_update(x_initial, omega_initial, time1, time2):
     #return updated values of period, acceleration, ang velocity, angular position
-    T = (2 * math.pi) * (math.sqrt(L / g))
-    theta = theta_max * (math.sin( math.sqrt(g / L))) * dt
-    acc_t = (g * math.sin(math.radians(theta))) /
-    vel_t = math.sqrt(2 * g * L * (1 - (math.cos(theta_r))))
-    return theta, vel_t, acc_t
+    dt = time2 - time1
+    T = (2 * np.pi) * (np.sqrt(L / g))
+    omega = omega_initial * math.cos(math.sqrt(g / L) * dt )
+    x_t = array_x * (np.sin(omega_initial * dt))
+    return x_t, omega
+
+def print_system(time, x_initial, omega_initial):
+    print("TIME:       ", time)
+    print("POSITION:   ", x_initial)
+    print("ANGLE:   ", omega_initial,  "\n")
 
 #initial conditions
-theta_max = [0]
-vel_t = [0]
-acc_t = [0, 4.9, 6.9, 8.49, 9.8, 9.8, 8.49, 6.9, 4.9, 0, -4.9, -6.9, -8.49, -9.8, -9.8, -8.49, -6.9, -4.9, 0]
-T = np.linspace(0, 20, 21)
-print_system(T[0], theta[0], vel_t[0], acc_t[0])
+x_initial = [0]
+array_x = np.array(x_initial)
+omega_initial = [0, math.pi/2, 20, 30, 40, math.pi / 3, math.pi / 4, math.pi / 6, math.pi]
+time = np.linspace(0, 20, 21)
+
+#vel_t = [0]
+#acc_t = [0, 4.9, 6.9, 8.49, 9.8, 9.8, 8.49, 6.9, 4.9, 0, -4.9, -6.9, -8.49, -9.8, -9.8, -8.49, -6.9, -4.9, 0]
+
+
+print_system(time[0], x_initial[0], omega_initial[0])
 
 i = 1
-while i < len(T):
-    theta, vel_t = system_update(acc_t[i], theta[i-1], vel_t[i-1], T[i-1], T[i])
-    theta.append(theta)
-    vel_t.append(vel_t)
-    print_system(T[i], theta[i], vel_t[i])
+while i < len(time):
+    x_t, omega = system_update(x_initial[i-1], omega_initial[i-1], time[i-1], time[i])
+    x_initial.append(x_t)
+    omega_initial.append(omega)
+    print_system(time[i], x_initial[i], omega_initial[i])
+    i += 1
