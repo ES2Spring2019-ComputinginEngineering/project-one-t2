aad98c35bd150f01e2b84d4bc901d4830e248b5f
ttinaguo <tina.guo@tufts.edu>
2019-02-20 23:26:49 -0500

    Update pendulum project.py

--- pendulum project.py
+++ pendulum project.py
@@ -10,38 +10,36 @@ import numpy as np
 L = .12 #m
 g = 9.8 #m/s^2
 m = 0.5 #kg
-theta_max = 90 #degrees
-theta = 172
-theta_r = math.radians(theta)
+time = np.linspace(0, 20, 21)
 
-def system_update(T, acc_t, vel_t, theta, t1, t2):
+
+def system_update(x_initial, time1, time2):
     #return updated values of period, acceleration, ang velocity, angular position
-    dt = t2 - t1
     T = (2 * math.pi) * (math.sqrt(L / g))
-    theta = theta_max * (math.sin( math.sqrt(g / L))) * dt
-    acc_t = (g * math.sin(math.radians(theta))) / m
-    vel_t = math.sqrt(2 * g * L * (1 - (math.cos(theta_r))))
-    return theta, vel_t, acc_t
+    omega = (2 * math.pi) / T
+    x_t = x_initial * math.sin(omega * time)
+    #acc_t = (g * math.sin(math.radians(theta))) / m
+    #vel_t = math.sqrt(2 * g * L * (1 - (math.cos(theta_r))))
+    return T, x_t #theta, vel_t, acc_t
 
 
-def print_system(T, acc_t, vel_t, theta):
-    print("TIME:        ", T)
-    print("POSITION:    ", theta)
-    print("VELOCITY:    ", vel_t)
-    print("ACCEL:       ", acc_t,  "\n")
+def print_system(time, x_t):#, acc_t, vel_t, theta):
+    print("TIME:          ", time)
+    print("POSITION:    ", x_t, "\n")
+    #print("VELOCITY:    ", vel_t)
+    #print("ACCEL:       ", acc_t,  "\n")
 
 
 #initial conditions
-theta = [0]
-vel_t = [0]
-acc_t = [0, 4.9, 6.9, 8.49, 9.8, 9.8, 8.49, 6.9, 4.9, 0, -4.9, -6.9, -8.49, -9.8, -9.8, -8.49, -6.9, -4.9, 0]
-T = np.linspace(0, 20, 21)
-print_system(T[0], theta[0], vel_t[0], acc_t[0])
+x_initial = [0]
+#vel_t = [0]
+#acc_t = [0, 4.9, 6.9, 8.49, 9.8, 9.8, 8.49, 6.9, 4.9, 0, -4.9, -6.9, -8.49, -9.8, -9.8, -8.49, -6.9, -4.9, 0]
+
+print_system(time[0], x_initial[0]) #theta[0], vel_t[0], acc_t[0])
 
 i = 1
-while i < len(T):
-    theta, vel_t = system_update(acc_t[i], theta[i-1], vel_t[i-1], T[i-1], T[i])
-    theta.append(theta)
-    vel_t.append(vel_t)
-    print_system(T[i], theta[i], vel_t[i])
+while i < len(time):
+    x_t = system_update(x_initial[i-1],  time[i-1], time[i])
+    x_initial.append(x_t)
+    print_system(time[i], x_initial[i])#, theta[i], vel_t[i])
     i += 1
