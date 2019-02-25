d9ecad2325ac1b4906b8ea238a067ff1d5da2b69
Amma Agyei <agyeiamma@gmail.com>
2019-02-25 15:40:45 -0500

    Update pendulum project.py

--- pendulum project.py
+++ pendulum project.py
@@ -22,25 +22,24 @@ def system_update(omega_initial, theta_initial, theta_t, time1, time2):
     acc_t = ((-1 * g) / L ) * math.sin(theta)
     return theta, acc_t, omega
 
-def print_system(time, omega_initial, acc_initial, theta_initial):
-    print("TIME:       ", time)
+def print_system(omega_initial, acc_initial, theta_initial):
     print("POSITION:   ", theta_initial)
     print("ACCELERATION:   ", acc_initial)
     print("VELOCITY: ", omega_initial, "\n")
-    print((time, theta_initial, acc_initial, omega_initial))
+    print((omega_initial, acc_initial, theta_initial))
 
 #initial conditions
-theta_initial = [math.pi]
-omega_initial = [5]
+theta_initial = [math.pi / 6]
+omega_initial = [0]
 acc_initial = [0]
-time = np.linspace(0, 20, 100)
-print_system(time[0], omega_initial[0], acc_initial[0], theta_initial[0])
+time = np.linspace(0, 20, 20000)
+print_system(omega_initial[0], acc_initial[0], theta_initial[0])
 
 i = 1
 while i < len(time):
-    acc_t, theta, omega = system_update(omega_initial[i-1], theta_initial[i-1], acc_initial[i-1], time[i-1], time[i])
+    omega, theta, acc_t = system_update(omega_initial[i-1], theta_initial[i-1], acc_initial[i-1], time[i-1], time[i])
     omega_initial.append(omega)
     theta_initial.append(theta)
     acc_initial.append(acc_t)
-    print_system(time[i], omega_initial[i], acc_initial[i], theta_initial[i])
+    print_system(omega_initial[i], acc_initial[i], theta_initial[i])
     i += 1
