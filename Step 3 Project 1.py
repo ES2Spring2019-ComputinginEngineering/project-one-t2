bb94d8f8c8594e936c58dac1581fcca02f3ed2bd
Amma Agyei <agyeiamma@gmail.com>
2019-02-26 16:16:26 -0500

    Create Step 3 Project 1.py

---
+++ Step 3 Project 1.py
@@ -0,0 +1,42 @@
+import microbit
+import math
+
+#Pseudocode
+#Calculate angle function
+#Stop watch function to know angle after a certain amount of time.
+#Be able to log time data with position data that is, as the position of microbit is changing, record the angle and the time at that angle.
+#Statement to begin logging of data, and statement to end logging of data.
+#Speed of at least 10Hz
+
+import microbit
+import os
+
+#Comma-Separated Values
+
+time0 = 0
+time1 = 0
+elapsed_time = time1 - time0
+
+def calculate_angle(x,y,z):
+        a1 = math.atan2(x,math.sqrt((y**2) + (z**2 )))
+        return math.degrees(a1)
+
+a = 0
+while True:
+    if a == 0:
+        if microbit.button_a.was_pressed():
+            while not microbit.button_b.was_pressed():
+                microbit.display.show(microbit.Image.ARROW_E)
+                time0 = microbit.running_time()
+                x = microbit.accelerometer.get_x()
+                y = microbit.accelerometer.get_y()
+                z = microbit.accelerometer.get_z()
+                x_angle = calculate_angle(x,y,z)
+                y_angle = calculate_angle(y,x,z)
+                print(time0)
+                print(y_angle)
+
+
+with open("data.txt.", "w+") as my_file:
+    my_file.write(time0)
+    my_file.write(y_angle)
