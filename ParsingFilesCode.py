6f38e26ce858349821d7caf0ef8a8837ead7e377
Amma Agyei <agyeiamma@gmail.com>
2019-03-06 00:50:40 -0500

    Update ParsingFilesCode.py

--- ParsingFilesCode.py
+++ ParsingFilesCode.py
@@ -2,32 +2,32 @@
 import os
 
 
-fin = open('/Users/ammaagyei/mu_code/Experiment1.txt')
-mytxt = fin.read()
+fin = open('/Users/ammaagyei/mu_code/Experiment1.txt')  #Opens saved file
+mytxt = fin.read()                                      #Reads saved file
 print(mytxt)
 t = []
 acc = []
 ang = []
-
 i = 0
 line = ""
 data = []
+
+
 for char in mytxt:
-    if char == "\n":
+    if char == "\n":                     #Checks for the new line character, and appends the data
         data.append(line)
         line = ""
     else:
         line = line + char
 for num in data:
-    if i % 2 == 0:
+    if i % 2 == 0:                      #if line is even, appends t list with the data
         t.append(float(num))
-    else:
+    else:                                      #if line is odd, appends splits odd line into acc list and ang list
         acc.append(float(num.split(",")[0]))
         ang.append(float(num.split(",")[1]))
 
     i += 1
 fin.close()
-
 
 
 
