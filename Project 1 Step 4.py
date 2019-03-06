20a3659da7a13a1f49263130a2e1bccb0ae9851d
Amma Agyei <agyeiamma@gmail.com>
2019-03-05 23:53:45 -0500

    Update Project 1 Step 4.py

--- Project 1 Step 4.py
+++ Project 1 Step 4.py
@@ -1,47 +1,33 @@
 #Tina Guo and Amma Agyei
 import os
 
-os
 
-"""myfile = open('/Users/ammaagyei/mu_code/accelerometer0.txt')
-mytxt = myfile.read()
-myfile.close()
+fin = open('/Users/ammaagyei/mu_code/Experiment1.txt')
+mytxt = fin.read()
 print(mytxt)
-print(type(mytxt))"""
+t = []
+acc = []
+ang = []
 
+i = 0
+line = ""
+data = []
+for char in mytxt:
+    if char == "\n":
+        data.append(line)
+        line = ""
+    else:
+        line = line + char
+for num in data:
+    if i % 2 == 0:
+        t.append(float(num))
+    else:
+        acc.append(float(num.split(",")[0]))
+        ang.append(float(num.split(",")[1]))
 
-#i = 1
-def time_and_angle_data():
-    fin = open('/Users/ammaagyei/mu_code/accelerometer0.txt')
-    mytxt = fin.read()
-    print(mytxt)
-    t = []
-    a = []
-    i = 0
-    line = ""
-    data = []
-    for char in mytxt:
-        if char == "\n":
-            data.append(line)
-            line = ""
-        else:
-            line = line + char
-    for num in data:
-        #num = data.strip()
-        if i % 2 == 0:
-            t.append(num)
-        else:
-            a.append(num)
-        i += 1
-    fin.close()
-    #return a , t
-    print("Angle=", a)
-    print("Time=", t)
+    i += 1
+fin.close()
 
-
-#Graph acceleration vs time:
-
-#Plug in acceleration into
 
 
 
